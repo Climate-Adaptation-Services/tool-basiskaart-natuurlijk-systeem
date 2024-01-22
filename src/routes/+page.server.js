0186a2962e426e86445a7fb201283b244db4b960
stichtingcas <daniel@climateadaptationservices.com>
2024-01-22 12:07:56 +0100

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,7 @@
 import { dsv } from 'd3'
 
 export function load(){
-  const data = dsv(',', "https://gist.githubusercontent.com/stichtingcas/e994cf9910b917efecdd393c161efc3e/raw/b892e3a87859cd3b8b3cc2f574f8d806bb3217b8/kansenbedreigingen-def.csv")
+  const data = dsv(',', "https://gist.githubusercontent.com/stichtingcas/e994cf9910b917efecdd393c161efc3e/raw/e0d689852982abcc23f35b5c06080b1c18d27864/kansenbedreigingen-def.csv")
   const meta = dsv(',', "https://gist.githubusercontent.com/stichtingcas/569b87d3f105afec3c7f2785a9cee8f1/raw/0bf7a90945a47baeb0cb84b8959b4e315e398617/kansendreigingen-meta.csv")
   return { data, meta };
 }
